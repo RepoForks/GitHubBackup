8ae4a5846cb55e066c6350546ac6304a9e65103a
Alessandro Aime <alessandro.aime@hotmail.com>
2017-10-17 02:23:20 +0300

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -4,5 +4,7 @@ USERNAME='your_username'
 PASSWORD='your_password'
 
 cd ~/Desktop && mkdir GitHub_Backup && cd GitHub_Backup
-curl -u $USERNAME:$PASSWORD https://api.github.com/user/repos | grep 'full_name' | grep $USERNAME | sed 's/.*"full_name": "//' | sed 's/",//' |
+
+curl -u $USERNAME:$PASSWORD https://api.github.com/user/repos |
+grep 'full_name' | grep $USERNAME | sed 's/.*"full_name": "//' | sed 's/",//' |
 while IFS= read -r LINE; do git clone https://github.com/$LINE; done
