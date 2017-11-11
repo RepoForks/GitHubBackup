e88baa538a33c22f5e7969526ce91e8182d5874b
Alessandro Aime <alessandro.aime@hotmail.com>
2017-11-11 12:57:44 +0200

    Implemented incremental updates

--- Script.sh
+++ Script.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-USERNAME='your_username'
-PASSWORD='your_password'
+USERNAME='your_github_username'
+PASSWORD='your_github_password'
+DIRECTORY='github_backup_directory'
 
-cd ~/Desktop && mkdir GitHub_Backup && cd GitHub_Backup
+cd "$DIRECTORY"
 
 curl -u $USERNAME:$PASSWORD https://api.github.com/user/repos |
-grep 'full_name' | grep $USERNAME | sed 's/.*"full_name": "//' | sed 's/",//' |
-while IFS= read -r LINE; do git clone https://github.com/$LINE; done
+grep 'full_name' | grep $USERNAME | sed 's/.*"full_name": "//' |
+sed 's/",//' | sed -e "s/$USERNAME\///g" | while IFS= read -r REPO; do
+  if [ -d ./$REPO ]
+  then
+    echo "$REPO: PULLING UPDATES" && cd $REPO && git pull origin && cd ..
+  else
+    echo "$REPO: CLONING" && git clone https://github.com/$USERNAME/$REPO
+  fi
+done
